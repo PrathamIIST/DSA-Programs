86cb8e337d83aa8a6a8766e9be971b221838bba0
PrathamIIST <pratham.upasanicse2024@indoreinstitute.com>
2025-09-01 11:26:27 +0530

    Add files via upload

---
+++ SumOfSquareOfNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,sum=0;
+
+    cout<<"Enter range: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        sum=sum+(i*i);
+    }
+    cout<<"The sum of first "<<n<<" natural numbers is "<<sum;
+}
